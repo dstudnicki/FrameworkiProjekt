0bb2b556e40b2f4f4cb490b1963994e0716023a0
dstudnicki <domistu@poczta.fm>
2025-01-14 20:44:49 +0100

    Initial commit

---
+++ frontend/src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page.tsx";
+import App from "./App.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+                errorElement: <ErrorPage />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
