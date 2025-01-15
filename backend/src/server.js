af42324c0a3b1f6cf53f599a6d91819d187eb9f7
dstudnicki <domistu@poczta.fm>
2025-01-15 16:24:07 +0100

    feat(backend): add connection with DB, configure server

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db_config");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
